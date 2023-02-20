4c615296c709915b383dac61a95c982d2ff6d6b6
CEEJ987 <123979158+CEEJ987@users.noreply.github.com>
2023-02-20 15:47:29 -0600

    Update Assignment

---
+++ Class-01lab.js
@@ -0,0 +1,28 @@
+function Questions () {
+    let a1 = prompt("Hello, what is your name?") ;
+    if (a1 === null) {
+        return; //break out of the function early
+    }
+    alert("Hello " + a1);
+    let a2 = prompt("How are you doing today?") ;
+    if (a2 === null) {
+        return; //break out of the function early
+    }
+    alert("It's good that you are " + a2 + " today, " + a1 )
+    let a3 = prompt("What is your favorite color?") ;
+    if (a3 === null) {
+        return; //break out of the function early
+    }
+    alert("That is a cool color " + a3, + " " + a1)
+    let a4 = prompt("What is your favorite food?") ;
+    if (a4 === null) {
+        return; //break out of the function early
+    }
+    alert("Would like to submit or purchase a recipe for " + a4 + " today, " + a1 )
+    
+}
+
+function Answer () {
+    let 
+}
+
